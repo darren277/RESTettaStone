0fcac52b9c7d45df91e713b0d7df4ea97b1d29f7
darren277 <darren277@yahoo.com>
2024-09-30 07:25:46 -0700

    Nginx and Makefile additions for SpringBoot app.

--- server/nginx/entrypoint.sh
+++ server/nginx/entrypoint.sh
@@ -2,7 +2,7 @@
 
 ldconfig
 
-envsubst '${NGINX_PORT},${CROWAPP_IP},${CROWAPP_PORT},${ASPNETAPP_IP},${ASPNETAPP_PORT}' < /usr/local/openresty/nginx/conf/nginx.conf.template > /usr/local/openresty/nginx/conf/nginx.conf
+envsubst '${NGINX_PORT},${CROWAPP_IP},${CROWAPP_PORT},${ASPNETAPP_IP},${ASPNETAPP_PORT},${SPRINGBOOTAPP_IP},${SPRINGBOOTAPP_PORT}' < /usr/local/openresty/nginx/conf/nginx.conf.template > /usr/local/openresty/nginx/conf/nginx.conf
 
 /usr/local/openresty/nginx/sbin/nginx -g 'daemon off;'
 while true; do sleep 1d; done
