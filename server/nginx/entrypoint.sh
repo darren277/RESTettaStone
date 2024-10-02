dccb0c4beecf8beb7ffa4454ab1d1b3e866825bd
darren277 <darren277@yahoo.com>
2024-10-02 07:31:40 -0700

    Makefile and Nginx config for Perl app.

--- server/nginx/entrypoint.sh
+++ server/nginx/entrypoint.sh
@@ -3,7 +3,7 @@
 ldconfig
 
 envsubst '${NGINX_PORT},${CROWAPP_IP},${CROWAPP_PORT},${ASPNETAPP_IP},${ASPNETAPP_PORT},\
-  ${SPRINGBOOTAPP_IP},${SPRINGBOOTAPP_PORT},${RAILSAPP_IP},${RAILSAPP_PORT}'\
+  ${SPRINGBOOTAPP_IP},${SPRINGBOOTAPP_PORT},${RAILSAPP_IP},${RAILSAPP_PORT},${PERLAPP_IP},${PERLAPP_PORT}'\
   < /usr/local/openresty/nginx/conf/nginx.conf.template > /usr/local/openresty/nginx/conf/nginx.conf
 
 /usr/local/openresty/nginx/sbin/nginx -g 'daemon off;'
