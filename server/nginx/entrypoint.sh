36b9bb54a0a19c43b339cd94e6968f8933f3a70a
darren277 <darren277@yahoo.com>
2024-10-11 07:49:43 -0700

    Makefile and Nginx configurations for Symfony app.

--- server/nginx/entrypoint.sh
+++ server/nginx/entrypoint.sh
@@ -18,6 +18,7 @@ envsubst '${NGINX_PORT},\
   ${VIBEAPP_IP},${VIBEAPP_PORT},\
   ${FSHARPAPP_IP},${FSHARPAPP_PORT},\
   ${PROLOGAPP_IP},${PROLOGAPP_PORT},\
+  ${SYMFONYAPP_IP},${SYMFONYAPP_PORT},\
   ${DUMMY_VAR}'\
   < /usr/local/openresty/nginx/conf/upstreams.conf.template > /usr/local/openresty/nginx/conf/upstreams.conf
 
