3d78cfc57b6bdd8e3ad4db6af256c8c7df003697
darren277 <darren277@yahoo.com>
2024-11-03 01:58:55 -0700

    Nginx configurations for React Fiber app.

--- server/nginx/entrypoint.sh
+++ server/nginx/entrypoint.sh
@@ -29,7 +29,10 @@ envsubst '${NGINX_PORT},\
   ${DUMMY_VAR}'\
   < /usr/local/openresty/nginx/conf/upstreams.conf.template > /usr/local/openresty/nginx/conf/upstreams.conf
 
-envsubst '${REACTAPP_IP},${REACTAPP_PORT},${VUEAPP_IP},${VUEAPP_PORT},${ANGULARAPP_IP},${ANGULARAPP_PORT}' < /usr/local/openresty/nginx/conf/upstreams-frontend.conf.template > /usr/local/openresty/nginx/conf/upstreams-frontend.conf
+envsubst '${REACTAPP_IP},${REACTAPP_PORT},\
+  ${VUEAPP_IP},${VUEAPP_PORT},\
+  ${REACTFIBERAPP_IP},${REACTFIBERAPP_PORT},\
+  ${ANGULARAPP_IP},${ANGULARAPP_PORT}' < /usr/local/openresty/nginx/conf/upstreams-frontend.conf.template > /usr/local/openresty/nginx/conf/upstreams-frontend.conf
 
 /usr/local/openresty/nginx/sbin/nginx -g 'daemon off;'
 while true; do sleep 1d; done
