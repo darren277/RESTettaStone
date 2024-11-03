981ee7f8e651dbbd01eb8e9834e4d9a019443a84
darren277 <darren277@yahoo.com>
2024-11-03 01:01:46 -0800

    Code for Gatsby app.

---
+++ frontend/gatsbyapp/src/templates/user-page.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+const UserPageTemplate = ({ data }) => {
+  const { user } = data;
+
+  return (
+    <div>
+      <h1>User Detail</h1>
+      <p>ID: {user.id}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+};
+
+export default UserPageTemplate;
+
+export const query = graphql`
+  query($id: String!) {
+    user(id: { eq: $id }) {
+      id
+      email
+    }
+  }
+`;
