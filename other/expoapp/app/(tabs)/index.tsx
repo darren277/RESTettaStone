3f580eab2dc400b035f9835e7433fd6d7906f211
darren277 <darren277@yahoo.com>
2024-11-06 01:16:54 -0800

    Added back end API URL fetch to Expo app.

--- other/expoapp/app/(tabs)/index.tsx
+++ other/expoapp/app/(tabs)/index.tsx
@@ -1,11 +1,21 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 
+import React, { useEffect } from 'react';
+
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+import { useApi } from '@/hooks/api/useApi';
+
 export default function HomeScreen() {
+  let [users, setUsers] = useApi();
+
+  if (!users) {
+      return <ThemedText>Loading...</ThemedText>;
+  };
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -20,6 +30,10 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
+      <ThemedText type="subtitle">Users</ThemedText>
+        {
+            users.map((user) => {return (<ThemedText key={user.id}>{user.email}</ThemedText>)})
+        }
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
