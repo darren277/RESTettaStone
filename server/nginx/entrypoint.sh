e04590d50cba9c6ce9e5a3428a71b682bf512b7b
darren277 <darren277@yahoo.com>
2024-11-08 01:03:30 -0800

    Nginx configurations for Fat Free app.

--- server/nginx/entrypoint.sh
+++ server/nginx/entrypoint.sh
@@ -28,6 +28,7 @@ envsubst '${NGINX_PORT},\
   ${DJANGOAPP_IP},${DJANGOAPP_PORT},\
   ${FIREBASEAPP_IP},${FIREBASEAPP_PORT},\
   ${TOMCATAPP_IP},${TOMCATAPP_PORT},\
+  ${FATFREEAPP_IP},${FATFREEAPP_PORT},\
   ${DUMMY_VAR}'\
   < /usr/local/openresty/nginx/conf/upstreams.conf.template > /usr/local/openresty/nginx/conf/upstreams.conf
 
