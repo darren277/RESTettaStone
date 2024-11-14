710f4f2cd902c08bd3c5f71d60b79cd3c2478941
darren277 <darren277@yahoo.com>
2024-11-14 05:26:59 -0800

    Switched from envsubst to sed for Prometheus config.

--- other/monitoring/prometheus/entrypoint.sh
+++ other/monitoring/prometheus/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# Replace variables in the Prometheus template
-envsubst < /etc/prometheus/prometheus.template.yml > /etc/prometheus/prometheus.yml
+# Replace placeholders in prometheus.template.yml with environment variables
+sed -i "s/__FLASKAPP_IP__/${FLASKAPP_IP}/g" /etc/prometheus/prometheus.template.yml
+sed -i "s/__FLASKAPP_PORT__/${FLASKAPP_PORT}/g" /etc/prometheus/prometheus.template.yml
 
-# Start Prometheus with provided arguments
+# Move the processed file to prometheus.yml
+cp /etc/prometheus/prometheus.template.yml /etc/prometheus/prometheus.yml
+
+# Start Prometheus with the correct configuration
 exec prometheus "$@"
