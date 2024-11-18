b883fc711a1fb0a6f5b0e46f962b6f5a6c81cdb2
darren277 <darren277@yahoo.com>
2024-11-18 04:09:48 -0800

    Adjusted Rails app to only return an array of users directly.

--- backend/railsapp/app/controllers/users_controller.rb
+++ backend/railsapp/app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
       def index
         users = User.all
 
-        render json: { users: users }
+        render json: users
       end
 
       def show
