8307cb6446e4c65662a9f5a31af0e80ac6155e9f
darren277 <darren277@yahoo.com>
2024-11-18 05:31:08 -0800

    Adjusted Django app URL.

--- backend/djangoapp/src/settings.py
+++ backend/djangoapp/src/settings.py
@@ -6,6 +6,8 @@ APP_NAME = 'djangoapp'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+APPEND_SLASH = True
+USE_X_FORWARDED_HOST = True
 
 def get_secret(setting):
     """Get the secret variable or return explicit exception."""
--- backend/djangoapp/src/urls.py
+++ backend/djangoapp/src/urls.py
@@ -7,5 +7,5 @@ from django.views.generic import TemplateView
 urlpatterns = i18n_patterns(
     #path('', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
-    path('users/', include('djangoapp.users.urls'))
+    path('djangoapp/', include('djangoapp.users.urls'))
 )
