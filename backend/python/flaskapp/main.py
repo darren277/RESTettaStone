3c06e12f451521f0bb54b5b5aeec93879d9e8c8d
darren277 <darren277@yahoo.com>
2024-11-30 15:13:06 -0800

    Added full CRUD functionality for the Flask app.

--- backend/python/flaskapp/main.py
+++ backend/python/flaskapp/main.py
@@ -43,7 +43,7 @@ class User(Reflected, Base):
 
 db = SQLAlchemy()
 
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -73,13 +73,41 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/users')
+@app.route('/users', methods=['GET', 'POST'])
 @cross_origin()
 def users():
+    if request.method == 'POST':
+        data = request.get_json()
+        email = data.get('email')
+        user = User(email=email)
+        db.session.add(user)
+        db.session.commit()
+        return user.json()
     users = User.query.all()
     data = {'users': [user.json() for user in users]}
     response = app.response_class(response=json.dumps(data['users']), status=200, mimetype='application/json')
     return response
+
+
+@app.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+@cross_origin()
+def user(id):
+    user = User.query.get(id)
+    if not user:
+        return 'User not found', 404
+    if request.method == 'GET':
+        return user.json()
+    elif request.method == 'PUT':
+        data = request.get_json()
+        email = data.get('email')
+        user.email = email
+        db.session.commit()
+        return user.json()
+    elif request.method == 'DELETE':
+        db.session.delete(user)
+        db.session.commit()
+        return user.json()
+
 
 @app.after_request
 def after_request(response):
