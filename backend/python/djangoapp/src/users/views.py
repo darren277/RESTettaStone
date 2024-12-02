d0d0bc4fda168fb07f917b3f8fda3038be184579
darren277 <darren277@yahoo.com>
2024-12-02 05:31:46 -0800

    Started on Django app CRUD.

--- backend/python/djangoapp/src/users/views.py
+++ backend/python/djangoapp/src/users/views.py
@@ -2,9 +2,48 @@ from django.http import JsonResponse
 
 from .models import User
 
-#def get_users(request, email):
-def get_users(request):
+def users(request):
+    if request.method == 'POST':
+        return post_user(request)
     #a_list = User.objects.filter(pub_date__year=year)
     users = User.objects.all()
     context = [user.toJSON() for user in users]
     return JsonResponse(context, safe=False)
+
+def get_user(request, id):
+    user = User.objects.get(id=id)
+    context = user.toJSON()
+    return JsonResponse(context, safe=False)
+
+def user(request, id):
+    if request.method == 'GET':
+        return get_user(request, id)
+    elif request.method == 'PUT':
+        return update_user(request, id)
+    elif request.method == 'DELETE':
+        return delete_user(request, id)
+    user = User.objects.get(id=id)
+    context = user.toJSON()
+    return JsonResponse(context, safe=False)
+
+def post_user(request):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    user = User(name=name, email=email)
+    user.save()
+    context = user.toJSON()
+    return JsonResponse(context, safe=False)
+
+def update_user(request, id):
+    user = User.objects.get(id=id)
+    user.name = request.POST.get('name')
+    user.email = request.POST.get('email')
+    user.save()
+    context = user.toJSON()
+    return JsonResponse(context, safe=False)
+
+def delete_user(request, id):
+    user = User.objects.get(id=id)
+    user.delete()
+    return JsonResponse({'status': 'success'}, safe=False)
+
