e0bd133d14e4a6625633739ae01c4c86abc1c303
darren277 <darren277@yahoo.com>
2024-12-20 10:20:25 -0800

    Added some routes to Actix app.

--- backend/rust/actixapp/src/routes.rs
+++ backend/rust/actixapp/src/routes.rs
@@ -8,6 +8,13 @@ pub fn config_services(cfg: &mut web::ServiceConfig) {
             .service(
                 web::resource("")
                     .route(web::get().to(user_controller::find_all))
-        )
-    );
+                    .route(web::post().to(user_controller::create))
+            )
+            .service(
+                web::resource("/{id}")
+                    .route(web::get().to(user_controller::find_by_id))
+                    .route(web::put().to(user_controller::update))
+                    .route(web::delete().to(user_controller::delete))
+            )
+        );
 }
