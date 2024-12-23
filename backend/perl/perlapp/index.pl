d3aad68ad15bd5d5445d954c41287fb65d517edc
darren277 <darren277@yahoo.com>
2024-12-23 07:13:54 -0800

    Remaining CRUD functionality for Perl app.

--- backend/perl/perlapp/index.pl
+++ backend/perl/perlapp/index.pl
@@ -19,7 +19,7 @@ my $password = $ENV{PG_PASS};
 
 my $connection_string = "dbi:Pg:database=$ENV{PG_DB};host=$ENV{PG_HOST};port=$ENV{PG_PORT}";
 
-$dbh = DBI->connect($connection_string, $username, $password, {AutoCommit => 0, RaiseError => 1, PrintError => 0});
+my $dbh = DBI->connect($connection_string, $username, $password, {AutoCommit => 0, RaiseError => 1, PrintError => 0});
 
 sub handle_request {
     my $self = shift;
@@ -68,35 +68,121 @@ sub resp_hello {
           $cgi->end_html;
 }
 
-sub to_json {
-    my $data = shift;
-    my $json = '[';
-    my $first = 1;
-    foreach my $row (@$data) {
-        $json .= ',' if !$first;
-        $json .= '{';
-        my $first_field = 1;
-        foreach my $field (keys %$row) {
-            $json .= ',' if !$first_field;
-            $json .= "\"$field\": \"$row->{$field}\"";
-            $first_field = 0;
-        }
-        $json .= '}';
-        $first = 0;
-    }
-    $json .= ']';
-    return $json;
-}
-
+# /users
 sub resp_users {
-    print "Content-Type: application/json\n";
-    print "Cache-Control: no-cache\n\n";
+    use CGI;
+    my $cgi = CGI->new;
 
-    my $sth = $dbh->prepare("SELECT * FROM users");
-    $sth->execute();
-    my $users = $sth->fetchall_arrayref({});
-    print to_json($users);
+    #print "Cache-Control: no-cache\n\n";
+
+    # check request method
+    if ($ENV{REQUEST_METHOD} eq 'POST') {
+        print STDERR "POST\n";
+        print STDERR "Content-Type: " . $ENV{CONTENT_TYPE} . "\n";
+        print STDERR "Content-Length: " . $ENV{CONTENT_LENGTH} . "\n";
+        print STDERR "Request body: " . $cgi->param('POSTDATA') . "\n";
+        # Request body: {"email": "test_email1@testing.com"}
+        print STDERR "ENV: " . to_json(\%ENV) . "\n";
+
+        my $post_data = $cgi->param('POSTDATA');
+        my $json = from_json($post_data);
+        my $email = $json->{email};
+
+        unless ($email) {
+            print "HTTP/1.0 404 Not Found\r\n";
+            print "Content-Type: application/json\n\n";
+            print to_json({ error => 'Email is required' });
+            return;
+        }
+
+        # Log the email for debugging purposes
+        print STDERR "email: $email\n";
+
+        my $sth = $dbh->prepare("INSERT INTO users (email) VALUES (?)");
+        $sth->execute($email);
+        $dbh->commit();
+        print "HTTP/1.0 200 OK\r\n";
+        print "Content-Type: application/json\n\n";
+        print to_json({ email => $email });
+    }
+    else { # Default to GET
+        my $sth = $dbh->prepare("SELECT * FROM users");
+        $sth->execute();
+        my $users = $sth->fetchall_arrayref({});
+        print "HTTP/1.0 200 OK\r\n";
+        print "Content-Type: application/json\n\n";
+        print to_json($users);
+    }
 }
+
+# /users/id
+sub resp_user {
+    my $cgi = CGI->new;
+    return if !ref $cgi;
+
+    my ($id) = $cgi->path_info() =~ m!/users/(\d+)!;
+
+    print STDERR "id: $id\n";
+
+    if ($ENV{REQUEST_METHOD} eq 'PUT') {
+        my $email = $cgi->param('email');
+
+        my $sth = $dbh->prepare("SELECT * FROM users WHERE id = ?");
+        $sth->execute($id);
+        my $user = $sth->fetchrow_hashref();
+        unless ($user) {
+            print "HTTP/1.0 404 Not Found\r\n";
+            print "Content-Type: application/json\n\n";
+            print to_json({ error => 'User not found' });
+            return;
+        }
+
+        $sth = $dbh->prepare("UPDATE users SET email = ? WHERE id = ?");
+        $sth->execute($email, $id);
+        $dbh->commit();
+        print "HTTP/1.0 200 OK\r\n";
+        print "Content-Type: application/json\n\n";
+        print to_json({ id => $id, email => $email });
+    }
+    elsif ($ENV{REQUEST_METHOD} eq 'DELETE') {
+        my $sth = $dbh->prepare("SELECT * FROM users WHERE id = ?");
+        $sth->execute($id);
+        my $user = $sth->fetchrow_hashref();
+        unless ($user) {
+            print "HTTP/1.0 404 Not Found\r\n";
+            print "Content-Type: application/json\n\n";
+            print to_json({ error => 'User not found' });
+            return;
+        }
+
+        $sth = $dbh->prepare("DELETE FROM users WHERE id = ?");
+        $sth->execute($id);
+        $dbh->commit();
+        print "HTTP/1.0 200 OK\r\n";
+        print "Content-Type: application/json\n\n";
+        print to_json({ id => $id });
+    }
+    elsif ($ENV{REQUEST_METHOD} eq 'GET') {
+        my $sth = $dbh->prepare("SELECT * FROM users WHERE id = ?");
+        $sth->execute($id);
+        my $user = $sth->fetchrow_hashref();
+        unless ($user) {
+            print "HTTP/1.0 404 Not Found\r\n";
+            print "Content-Type: application/json\n\n";
+            print to_json({ error => 'User not found' });
+            return;
+        }
+        print "HTTP/1.0 200 OK\r\n";
+        print "Content-Type: application/json\n\n";
+        print to_json($user);
+    }
+    else {
+        print "HTTP/1.0 405 Method Not Allowed\r\n";
+        print "Content-Type: application/json\n\n";
+        print to_json({ error => 'Invalid request method' });
+    }
+}
+
 }
 
 # start the server on port 3012
