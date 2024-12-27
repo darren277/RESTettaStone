d13ba99d26475c4d49a293899b9bf0a1ab4191ce
darren277 <darren277@yahoo.com>
2024-12-27 08:31:57 -0800

    Converted Angular app indentation.

--- frontend/angularapp/src/app/app.component.ts
+++ frontend/angularapp/src/app/app.component.ts
@@ -5,43 +5,35 @@ import { environment } from '../environments/environment';
 
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
 })
 
 export class AppComponent {
-  title = "spMgAngDocker";
+    title = "spMgAngDocker";
 
-  constructor(private http: HttpClient) {}
+constructor(private http: HttpClient) {}
 
-  data = [];
+    data = [];
 
-  ngOnInit() {
-    this.getData();
-  }
-
-  getData() {
-    let url = `${environment.apiBaseUrl}/users`;
-    this.http.get(url).subscribe((res) => {
-      console.log(res);
-      this.data = res as any[];
-    });
-  }
-
-  addItem() {
-    throw new Error('Not implemented');
-    let url = "http://localhost:8080/addItem";
-    let data = { itemName: "new item" };
-    this.http
-      .post(url, data, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .subscribe((res) => {
-        console.log(res);
+    ngOnInit() {
         this.getData();
-      });
-  }
+    }
+
+    getData() {
+        let url = `${environment.apiBaseUrl}/users`;
+        this.http.get(url).subscribe((res) => {
+            console.log(res);
+            this.data = res as any[];
+        });
+    }
+
+    addItem() {
+        throw new Error('Not implemented');
+        let url = "http://localhost:8080/addItem";
+        let data = { itemName: "new item" };
+        this.http.post(url, data, {headers: {"Content-Type": "application/json"}})
+            .subscribe((res) => {console.log(res); this.getData();});
+    }
 }
