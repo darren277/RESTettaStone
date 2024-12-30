05caf706c03dc98e619b055b9b9b7bc1dac1ad1a
darren277 <darren277@yahoo.com>
2024-12-30 09:12:14 -0800

    Fixed socat call.

--- backend/bash/bashapp/server.sh
+++ backend/bash/bashapp/server.sh
@@ -4,4 +4,4 @@ PORT=3039
 DB_CONNECTION="postgresql://myusername:mypassword@172.18.0.21:5432/postgres"
 
 echo "Starting server on port $PORT..."
-/usr/bin/socat TCP-LISTEN:$PORT,reuseaddr,fork EXEC:"/bin/bash -c 'handle_request'"
+/usr/bin/socat TCP-LISTEN:$PORT,reuseaddr,fork EXEC:/usr/local/bin/request_handler.sh
