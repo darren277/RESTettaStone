1a861861a9cbfec420ce9dec1e4c411d1772cc1f
darren277 <darren277@yahoo.com>
2025-01-01 05:48:52 -0800

    Remaining CRUD for Chalice app.

--- other/chaliceapp/app.py
+++ other/chaliceapp/app.py
@@ -2,13 +2,70 @@
 import os
 
 from chalice import Chalice
+from chalice import NotFoundError
 import boto3 as boto3
 
 app = Chalice(app_name="chaliceapp")
 
-@app.route("/api/users")
-def index():
+def generate_id():
+    import uuid
+    return str(uuid.uuid4())
+
+def get_users():
     db = boto3.resource('dynamodb')
     table = db.Table(os.environ['USERS_TABLE'])
     users = [dict(id=item['ID'], email=item['email']) for item in table.scan().get('Items', [])]
     return users
+
+def create_user(user):
+    db = boto3.resource('dynamodb')
+    table = db.Table(os.environ['USERS_TABLE'])
+
+    user['ID'] = generate_id()
+
+    table.put_item(Item=user)
+    return user
+
+def get_user(user_id):
+    db = boto3.resource('dynamodb')
+    table = db.Table(os.environ['USERS_TABLE'])
+
+    user = table.get_item(Key={'ID': user_id}).get('Item')
+    if not user:
+        return NotFoundError("User not found")
+
+    return user
+
+def delete_user(user_id):
+    db = boto3.resource('dynamodb')
+    table = db.Table(os.environ['USERS_TABLE'])
+
+    if not table.get_item(Key={'ID': user_id}).get('Item'):
+        return NotFoundError("User not found")
+
+    table.delete_item(Key={'ID': user_id})
+    return {"status": "success", "id": user_id}
+
+def update_user(user_id, user):
+    db = boto3.resource('dynamodb')
+    table = db.Table(os.environ['USERS_TABLE'])
+
+    if not table.get_item(Key={'ID': user_id}).get('Item'):
+        return NotFoundError("User not found")
+
+    table.update_item(Key={'ID': user_id}, UpdateExpression='SET email = :val1', ExpressionAttributeValues={':val1': user['email']})
+    return user
+
+@app.route("/users/{user_id}", methods=["GET", "PUT", "DELETE"], content_types=['application/json'])
+def user(user_id):
+    if app.current_request.method == "PUT":
+        return update_user(user_id, app.current_request.json_body)
+    if app.current_request.method == "DELETE":
+        return delete_user(user_id)
+    return get_user(user_id)
+
+@app.route("/users", methods=["GET", "POST"], content_types=['application/json'])
+def index():
+    if app.current_request.method == "POST":
+        return create_user(app.current_request.json_body)
+    return get_users()
