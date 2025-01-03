36ea81d3d6074026bc45de7c3ae53b2e69929a4d
darren277 <darren277@yahoo.com>
2025-01-03 07:52:34 -0800

    Various gRPC server and client adjustments and fixes.

--- other/grpc/entrypoint-client.sh
+++ other/grpc/entrypoint-client.sh
@@ -4,6 +4,7 @@ go mod vendor
 
 # Note that this may not be ideal (copying the protos to the go src directory).
 # Keeping it for testing purposes at least.
-cp -r /protos /usr/local/go/src/
+mkdir -p /usr/local/go/src/protos/gen/go/user/v1
+cp -r /protos/go/user/v1 /usr/local/go/src/protos/gen/go/user/
 
 go build -o grpc-client client.go
