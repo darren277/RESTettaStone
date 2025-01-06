93267105665cc7bad87a5c00c7d363cdc23223ff
darren277 <darren277@yahoo.com>
2025-01-06 07:54:51 -0800

    Moved route definitions from config file to router.

--- backend/clojure/kitapp/src/user_service/router.clj
+++ backend/clojure/kitapp/src/user_service/router.clj
@@ -1,5 +1,6 @@
 (ns user_service.router
   (:require
+    [user_service.handler :as h]
     [integrant.core :as ig]
     [reitit.ring :as ring]
     [reitit.ring.middleware.muuntaja :as muuntaja]
@@ -8,7 +9,14 @@
 (defmethod ig/init-key :user_service.router/router
   [_ {:keys [routes]}]
   (ring/ring-handler
-    (ring/router routes
-                 {:data {:muuntaja   m/instance
-                         :middleware [muuntaja/format-middleware]}})
+    (ring/router
+        [["/users"
+            {:get  h/list-users
+             :post h/create-user}]
+           ["/users/:id"
+            {:get    h/get-user
+             :put    h/update-user
+             :delete h/delete-user}]]
+        {:data {:muuntaja   m/instance
+             :middleware [muuntaja/format-middleware]}})
     (ring/create-default-handler)))
