0c0bec2a8d85af54a861fc1195c9b7e1abdef056
darren277 <darren277@yahoo.com>
2025-01-06 07:11:47 -0800

    Simplified Kit app router.

--- backend/clojure/kitapp/src/user_service/router.clj
+++ backend/clojure/kitapp/src/user_service/router.clj
@@ -3,22 +3,12 @@
     [integrant.core :as ig]
     [reitit.ring :as ring]
     [reitit.ring.middleware.muuntaja :as muuntaja]
-    [reitit.coercion.malli]
-    [reitit.ring.coercion :as rrc]
     [muuntaja.core :as m]))
 
 (defmethod ig/init-key :router
   [_ {:keys [routes]}]
-  (let [defaults
-        {:data
-         {
-          ;; :coercion   malli/coercion
-          :muuntaja   m/instance
-          :middleware [muuntaja/format-middleware
-                       rrc/coerce-exceptions-middleware
-                       rrc/coerce-request-middleware
-                       rrc/coerce-response-middleware]}}]
-    ;; Return a Ring handler built by reitit
-    (ring/ring-handler
-      (ring/router routes defaults)
-      (ring/create-default-handler))))
+  (ring/ring-handler
+    (ring/router routes
+                 {:data {:muuntaja   m/instance
+                         :middleware [muuntaja/format-middleware]}})
+    (ring/create-default-handler)))
