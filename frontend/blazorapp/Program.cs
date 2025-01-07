5a57682ea4c6362c5fc00e70053c532b2c5f61d0
darren277 <darren277@yahoo.com>
2025-01-07 07:10:19 -0800

    Updated central Program file to use pure WebAssembly instead of Server.

--- frontend/blazorapp/Program.cs
+++ frontend/blazorapp/Program.cs
@@ -1,43 +1,26 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorApp;
 using BlazorApp.Services;
 using BlazorApp.Models;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+// If you have a component called <App /> at the root (App.razor):
+builder.RootComponents.Add<App>("#app");
 
 var NGINX_HOST = Environment.GetEnvironmentVariable("NGINX_HOST");
 var NGINX_PORT = Environment.GetEnvironmentVariable("NGINX_PORT");
 var FLASK_APP = $"http://{NGINX_HOST}:{NGINX_PORT}/flaskapp";
 var USER_API_BASE_URL = $"{FLASK_APP}/users";
 
-builder.Services.AddHttpClient("FlaskApi", client =>
+builder.Services.AddScoped(sp => new HttpClient
 {
-    client.BaseAddress = new Uri(FLASK_APP);
+    BaseAddress = new Uri(FLASK_APP)
 });
 
 builder.Services.AddScoped<UserService>();
 
 string port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
-var app = builder.Build();
-
-app.MapGet("/users", async (UserService service) =>
-    await service.GetAllUsersAsync());
-
-app.MapGet("/users/{id:int}", async (int id, UserService service) =>
-    await service.GetUserByIdAsync(id));
-
-app.MapPost("/users", async (User user, UserService service) =>
-    await service.CreateUserAsync(user));
-
-app.MapPut("/users/{id:int}", async (int id, User user, UserService service) =>
-    await service.UpdateUserAsync(id, user));
-
-app.MapDelete("/users/{id:int}", async (int id, UserService service) =>
-    await service.DeleteUserAsync(id));
-
-app.MapBlazorHub();
-
-app.Run();
+await builder.Build().RunAsync();
