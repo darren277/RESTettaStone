4b7f98e68bfb9daf0b451567aa6b1da63e483551
darren277 <darren277@yahoo.com>
2025-01-07 07:50:46 -0800

    Added proper client side env var usage.

--- frontend/blazorapp/Program.cs
+++ frontend/blazorapp/Program.cs
@@ -9,10 +9,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 // If you have a component called <App /> at the root (App.razor):
 builder.RootComponents.Add<App>("#app");
 
-var NGINX_HOST = Environment.GetEnvironmentVariable("NGINX_HOST");
-var NGINX_PORT = Environment.GetEnvironmentVariable("NGINX_PORT");
+var NGINX_HOST = builder.Configuration["NGINX_HOST"];
+var NGINX_PORT = builder.Configuration["NGINX_PORT"];
 var FLASK_APP = $"http://{NGINX_HOST}:{NGINX_PORT}/flaskapp";
 var USER_API_BASE_URL = $"{FLASK_APP}/users";
+
+Console.WriteLine($"NGINX_HOST={NGINX_HOST}");
+Console.WriteLine($"NGINX_PORT={NGINX_PORT}");
+Console.WriteLine($"FLASK_APP={FLASK_APP}");
+Console.WriteLine($"USER_API_BASE_URL={USER_API_BASE_URL}");
 
 builder.Services.AddScoped(sp => new HttpClient
 {
