597b079f3242afe870ad414ededbe838275a79e2
darren277 <darren277@yahoo.com>
2025-01-19 08:29:14 -0800

    Adjusted indenting for better readability.

--- frontend/flutterapp/lib/main.dart
+++ frontend/flutterapp/lib/main.dart
@@ -2,16 +2,12 @@ import 'package:flutter/material.dart';
 import 'screens/user_screen.dart';
 
 void main() {
-  runApp(MyApp());
+    runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter REST API Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: UserScreen(),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(title: 'Flutter REST API Demo', theme: ThemeData(primarySwatch: Colors.blue), home: UserScreen());
+    }
 }
--- frontend/flutterapp/lib/services/api_service.dart
+++ frontend/flutterapp/lib/services/api_service.dart
@@ -4,61 +4,56 @@ import 'package:http/http.dart' as http;
 import '../models/user.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:8084/flaskapp'; // Replace with your API URL
+    static const String baseUrl = 'http://localhost:8084/flaskapp'; // Replace with your API URL
 
-  Future<List<User>> getUsers() async {
-    final response = await http.get(Uri.parse('$baseUrl/users'));
-    log('response: ${response.body}');
-    if (response.statusCode == 200) {
-      final List<dynamic> data = jsonDecode(response.body);
-      return data.map((json) => User.fromJson(json)).toList();
-    } else {
-      throw Exception('Failed to load users');
+    Future<List<User>> getUsers() async {
+        final response = await http.get(Uri.parse('$baseUrl/users'));
+
+        log('response: ${response.body}');
+        if (response.statusCode == 200) {
+            final List<dynamic> data = jsonDecode(response.body);
+            return data.map((json) => User.fromJson(json)).toList();
+        } else {
+            throw Exception('Failed to load users');
+        }
     }
-  }
 
-  Future<User> createUser(String email) async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/users'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'email': email}),
-    );
+    Future<User> createUser(String email) async {
+        final response = await http.post(Uri.parse('$baseUrl/users'), headers: {'Content-Type': 'application/json'}, body: jsonEncode({'email': email}));
 
-    if (response.statusCode == 200) {
-      return User.fromJson(jsonDecode(response.body));
-    } else {
-      log('data: ${response.body}');
-      throw Exception('Failed to create user');
+        if (response.statusCode == 200) {
+            return User.fromJson(jsonDecode(response.body));
+        } else {
+            log('data: ${response.body}');
+            throw Exception('Failed to create user');
+        }
     }
-  }
 
-  Future<User> getUserById(int id) async {
-    final response = await http.get(Uri.parse('$baseUrl/users/$id'));
-    if (response.statusCode == 200) {
-      return User.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('User not found');
+    Future<User> getUserById(int id) async {
+        final response = await http.get(Uri.parse('$baseUrl/users/$id'));
+
+        if (response.statusCode == 200) {
+            return User.fromJson(jsonDecode(response.body));
+        } else {
+            throw Exception('User not found');
+        }
     }
-  }
 
-  Future<User> updateUser(int id, String email) async {
-    final response = await http.put(
-      Uri.parse('$baseUrl/users/$id'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'email': email}),
-    );
+    Future<User> updateUser(int id, String email) async {
+        final response = await http.put(Uri.parse('$baseUrl/users/$id'), headers: {'Content-Type': 'application/json'}, body: jsonEncode({'email': email}));
 
-    if (response.statusCode == 200) {
-      return User.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to update user');
+        if (response.statusCode == 200) {
+            return User.fromJson(jsonDecode(response.body));
+        } else {
+            throw Exception('Failed to update user');
+        }
     }
-  }
 
-  Future<void> deleteUser(int id) async {
-    final response = await http.delete(Uri.parse('$baseUrl/users/$id'));
-    if (response.statusCode != 200) {
-      throw Exception('Failed to delete user');
+    Future<void> deleteUser(int id) async {
+        final response = await http.delete(Uri.parse('$baseUrl/users/$id'));
+
+        if (response.statusCode != 200) {
+            throw Exception('Failed to delete user');
+        }
     }
-  }
 }
