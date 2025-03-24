74645ce7a90434f869bc2181c27665eb4f9c2586
darren277 <darren277@yahoo.com>
2025-03-24 05:59:31 -0700

    Using env vars for Postgres connection.

--- backend/julia/genieapp/src/app.jl
+++ backend/julia/genieapp/src/app.jl
@@ -11,8 +11,15 @@ route("/json") do
 end
 
 # GET /users - fetch an array of all users
-route("/users", method="GET") do
-    conn = LibPQ.Connection("dbname=postgres host=172.18.0.21 port=5432 user=myusername password=mypassword")
+route("/users", method="GET")
+    host = get(ENV, "PG_HOST", "localhost")
+    port = get(ENV, "PG_PORT", "5432")
+    user = get(ENV, "PG_USER", "postgres")
+    pass = get(ENV, "PG_PASS", "")
+    db = get(ENV, "PG_DB", "postgres")
+
+    conn_string = "dbname=$db host=$host port=$port user=$user password=$pass"
+    conn = LibPQ.Connection(conn_string)
     result = execute(conn, "SELECT * FROM users")
     data = columntable(result)
     close(conn)
